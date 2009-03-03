a317844b2d2bd6823b8b767c621f63f6899eba55
Jeremy Thomas <SND\JezzaT_cp>
2009-03-03 11:21:39 +0000

    Started to add the ability to script queries like in LinqPad

--- href.controls/EntityViewer/FrmEntityViewer.cs
+++ href.controls/EntityViewer/FrmEntityViewer.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Windows.Forms;
 using AW.Winforms.Helpers.Properties;
+using CSScriptLibrary;
 using DynamicTable;
 using SD.LLBLGen.Pro.ApplicationCore;
 using SD.LLBLGen.Pro.ApplicationCore.Entities;
@@ -14,7 +15,7 @@ using WB.LLBLGen.Pro.Plugins;
 
 namespace AW.Winforms.Helpers.EntityViewer
 {
-  public partial class FrmEntityViewer: Form
+  public partial class FrmEntityViewer : Form
   {
     private static TypeDescriptionProvider CommonEntityBaseTypeDescriptionProvider;
     private static TypeDescriptionProvider EntityFieldsTypeDescriptionProvider; //
@@ -63,7 +64,7 @@ namespace AW.Winforms.Helpers.EntityViewer
         if (!(e.NewSelection.Value is string))
           if (e.NewSelection.Value is IEnumerable)
           {
-            var enumerable = (IEnumerable)e.NewSelection.Value;
+            var enumerable = (IEnumerable) e.NewSelection.Value;
             try
             {
               bindingSourceEnumerable.DataSource = enumerable.AsQueryable();
@@ -93,48 +94,85 @@ namespace AW.Winforms.Helpers.EntityViewer
     {
       bindingSourceEnumerable.DataSource = propertyGrid1.SelectedObject;
       if (propertyGrid1.SelectedObject is IEntity)
-        entityFieldBindingSource.DataSource = ((IEntity)propertyGrid1.SelectedObject).Fields.AsQueryable();
+        entityFieldBindingSource.DataSource = ((IEntity) propertyGrid1.SelectedObject).Fields.AsQueryable();
       if (propertyGrid1.SelectedObject is Project)
       {
         Text = "ProjectBrowser";
         dataGridViewFields.AutoGenerateColumns = true;
-        entityFieldBindingSource.DataSource = ((Project)propertyGrid1.SelectedObject).Entities;
+        entityFieldBindingSource.DataSource = ((Project) propertyGrid1.SelectedObject).Entities;
         toolStripButtonViewGroupedEntities.Visible = true;
       }
     }
 
     private Project TheProject
     {
-      get { return (Project)propertyGrid1.SelectedObject; }
+      get { return (Project) propertyGrid1.SelectedObject; }
     }
 
     private void toolStripButtonViewGroupedEntities_Click(object sender, EventArgs e)
     {
-      var x = from entity in TheProject.Entities.OfType<EntityDefinition>()
-              select
-                new
-                  {
-                    entity.ElementTargetName,
-                    entity.ElementName,
-                    entity.TargetType,
-                    groups = string.Join(",", (from peg in TheProject.ProjectElementGroups.Values.OfType<ProjectElementGroup>()
-                                               from ed in peg.Keys.OfType<EntityDefinition>()
-                                               where !peg.IsSystemGroup && ed.ElementTargetName == entity.ElementTargetName
-                                               select peg.Name).ToArray()),
-                    Description = entity.CustomProperties.ContainsKey("MS_Description") ? entity.CustomProperties["MS_Description"].ToString() : string.Empty
-                    //.Aggregate((current, next) => current + ", " + next)
-                  };
-      var t = from peg in TheProject.ProjectElementGroups.Values.OfType<ProjectElementGroup>()
-              from ed in peg.Keys.OfType<EntityDefinition>()
-              where !peg.IsSystemGroup && ed.ElementTargetName == "AQD.gn_Aircraft"
-              select new {ed.ElementTargetName, ed.ElementName, peg.Name};
-      bindingSourceEnumerable.DataSource = x;
+      //var t = from peg in TheProject.ProjectElementGroups.Values.OfType<ProjectElementGroup>()
+      //        from ed in peg.Keys.OfType<EntityDefinition>()
+      //        where !peg.IsSystemGroup && ed.ElementTargetName == "AQD.gn_Aircraft"
+      //        select new {ed.ElementTargetName, ed.ElementName, peg.Name};
+
+      AsmHelper myAsmHelper;
+      var query = CreateQuery(out myAsmHelper);
+      bindingSourceEnumerable.DataSource = query.QueryProject(TheProject);
+      myAsmHelper.Dispose();
+      //bindingSourceEnumerable.DataSource = GetEntityTableMapping(TheProject);
     }
+
+    private static IEnumerable GetEntityTableMapping(Project project)
+    {
+      return from entity in project.Entities.OfType<EntityDefinition>()
+             select
+               new
+                 {
+                   entity.ElementTargetName,
+                   entity.ElementName,
+                   entity.TargetType,
+                   groups = string.Join(",", (from peg in project.ProjectElementGroups.Values.OfType<ProjectElementGroup>()
+                                              from ed in peg.Keys.OfType<EntityDefinition>()
+                                              where !peg.IsSystemGroup && ed.ElementTargetName == entity.ElementTargetName
+                                              select peg.Name).ToArray()),
+                   Description = entity.CustomProperties.ContainsKey("MS_Description") ? entity.CustomProperties["MS_Description"].ToString() : string.Empty
+                   //.Aggregate((current, next) => current + ", " + next)
+                 };
+    }
+
+    private const string GetEntityTableMappingTemplate = @"using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+using AW.Winforms.Helpers.Properties;
+using DynamicTable;
+using SD.LLBLGen.Pro.ApplicationCore;
+using SD.LLBLGen.Pro.ApplicationCore.Entities;
+
+public class Script : MarshalByRefObject, IQueryScript
+{
+    public IEnumerable QueryProject(Project project);
+    {
+      return from entity in project.Entities.OfType<EntityDefinition>()
+             select
+               new
+                 {
+                   entity.ElementTargetName,
+                   entity.ElementName,
+                   entity.TargetType,
+                 };
+    }
+
+}";
 
     private void toolStripButtonRunPlugin_Click(object sender, EventArgs e)
     {
       //PluginBase pluginToTest = new RenameRelatedFieldsPlugin {ProjectToTarget = TheProject, Callbacks = new Hashtable {{ProgressCallBack.LogLineToApplicationOutputCallBack, (ApplicationOutputLogLineCallBack)ApplicationOutputLogLine}}};
-      PluginBase pluginToTest = new RefreshCustomProperties {ProjectToTarget = TheProject, Callbacks = new Hashtable {{ProgressCallBack.LogLineToApplicationOutputCallBack, (ApplicationOutputLogLineCallBack)ApplicationOutputLogLine}}};
+      PluginBase pluginToTest = new RefreshCustomProperties {ProjectToTarget = TheProject, Callbacks = new Hashtable {{ProgressCallBack.LogLineToApplicationOutputCallBack, (ApplicationOutputLogLineCallBack) ApplicationOutputLogLine}}};
       pluginToTest.Entities.AddRange(TheProject.Entities);
       pluginToTest.GetConfigurationControl();
       pluginToTest.Execute();
@@ -150,9 +188,20 @@ namespace AW.Winforms.Helpers.EntityViewer
 
     private void toolStripButtonViewReport_Click(object sender, EventArgs e)
     {
-      var frm = new FrmReportViewer {MdiParent = this.MdiParent, WindowState = FormWindowState.Normal};
+      var frm = new FrmReportViewer {MdiParent = MdiParent, WindowState = FormWindowState.Normal};
       frm.OpenDataSet(bindingSourceEnumerable, false);
       frm.Show();
     }
+
+    private static IQueryScript CreateQuery(out AsmHelper helper)
+    {
+      helper = new AsmHelper(CSScript.LoadCode(GetEntityTableMappingTemplate, null, true));
+      return (IQueryScript) helper.CreateObject("Script");
+    }
+  }
+
+  public interface IQueryScript
+  {
+    IEnumerable QueryProject(Project project);
   }
 }
