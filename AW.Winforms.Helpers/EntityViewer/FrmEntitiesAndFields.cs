b26bc2f20bb9c348580aff16f3cb93e61958eb67
Jeremy Thomas <SND\JezzaT_cp>
2009-08-04 13:01:53 +0000

    Now shows the properties of type entity since for selfservicing these properties are not browsable so they need to be handled as a special case.

--- AW.Winforms.Helpers/EntityViewer/FrmEntitiesAndFields.cs
+++ AW.Winforms.Helpers/EntityViewer/FrmEntitiesAndFields.cs
@@ -11,114 +11,137 @@ using SD.LLBLGen.Pro.ORMSupportClasses;
 
 namespace AW.Winforms.Helpers.EntityViewer
 {
-	public partial class FrmEntitiesAndFields: Form
-	{
-		private static FrmEntitiesAndFields _formSingleton;
-		private readonly Type _baseType;
-		private readonly Assembly _entityAssembly;
+  public partial class FrmEntitiesAndFields : Form
+  {
+    private static FrmEntitiesAndFields _formSingleton;
+    private readonly Type _baseType;
+    private readonly Assembly _entityAssembly;
 
-		public FrmEntitiesAndFields()
-		{
-			InitializeComponent();
-		}
+    public FrmEntitiesAndFields()
+    {
+      InitializeComponent();
+    }
 
-		public FrmEntitiesAndFields(Assembly entityAssembly): this()
-		{
-			_entityAssembly = entityAssembly;
-		}
+    public FrmEntitiesAndFields(Assembly entityAssembly) : this()
+    {
+      _entityAssembly = entityAssembly;
+    }
 
-		public FrmEntitiesAndFields(Type baseType): this(baseType.Assembly)
-		{
-			_baseType = baseType;
-		}
+    public FrmEntitiesAndFields(Type baseType) : this(baseType.Assembly)
+    {
+      _baseType = baseType;
+    }
 
-		public static void ShowEntitiesAndFields(Type baseType)
-		{
-			ShowEntitiesAndFields(baseType, null);
-		}
+    public static void ShowEntitiesAndFields(Type baseType)
+    {
+      ShowEntitiesAndFields(baseType, null);
+    }
 
-		public static void ShowEntitiesAndFields(Type baseType, Form parent)
-		{
-			if (_formSingleton == null)
-				_formSingleton = new FrmEntitiesAndFields(baseType);
-			_formSingleton.WindowState = FormWindowState.Normal;
-			if (parent == null)
-				_formSingleton.ShowDialog();
-			else
-			{
-				_formSingleton.Parent = parent;
-				_formSingleton.Show();
-			}
-		}
+    public static void ShowEntitiesAndFields(Type baseType, Form parent)
+    {
+      if (_formSingleton == null)
+        _formSingleton = new FrmEntitiesAndFields(baseType);
+      _formSingleton.WindowState = FormWindowState.Normal;
+      if (parent == null)
+        _formSingleton.ShowDialog();
+      else
+      {
+        _formSingleton.Parent = parent;
+        _formSingleton.Show();
+      }
+    }
 
-		private void FrmEntitiesAndFields_FormClosed(object sender, FormClosedEventArgs e)
-		{
-			Settings.Default.EntitiesAndFieldsSizeLocation = AWHelper.GetWindowNormalSizeAndLocation(this);
-			Settings.Default.Save();
-		}
+    private void FrmEntitiesAndFields_FormClosed(object sender, FormClosedEventArgs e)
+    {
+      Settings.Default.EntitiesAndFieldsSizeLocation = AWHelper.GetWindowNormalSizeAndLocation(this);
+      Settings.Default.Save();
+    }
 
-		private void EntitiesAndFields_Load(object sender, EventArgs e)
-		{
-			AWHelper.SetWindowSizeAndLocation(this, Settings.Default.EntitiesAndFieldsSizeLocation);
-			treeViewEntities.Nodes.Clear();
-			foreach (var entityType in GetEntitiesTypes().OrderBy(t => t.Name))
-			{
-				var entityNode = treeViewEntities.Nodes.Add(entityType.Name, entityType.Name.Replace("Entity", ""));
+    private void EntitiesAndFields_Load(object sender, EventArgs e)
+    {
+      AWHelper.SetWindowSizeAndLocation(this, Settings.Default.EntitiesAndFieldsSizeLocation);
+      treeViewEntities.Nodes.Clear();
+      foreach (var entityType in GetEntitiesTypes().OrderBy(t => t.Name))
+      {
+        var entityNode = treeViewEntities.Nodes.Add(entityType.Name, entityType.Name.Replace("Entity", ""));
 
-				foreach (var browsableProperty in ListBindingHelper.GetListItemProperties(entityType).Cast<PropertyDescriptor>().
-					Where(p => !typeof (IList).IsAssignableFrom(p.PropertyType)).OrderBy(p => p.Name))
-				{
-					var fieldNode = entityNode.Nodes.Add(browsableProperty.Name);
-					if (typeof(IEntityCore).IsAssignableFrom(browsableProperty.PropertyType))
-					{
-						fieldNode.ImageIndex = 3;
-						fieldNode.Tag = browsableProperty;
-					}
-					else
-						fieldNode.ImageIndex = 1;
-				}
+        foreach (var browsableProperty in ListBindingHelper.GetListItemProperties(entityType).Cast<PropertyDescriptor>().
+          Where(p => !typeof (IList).IsAssignableFrom(p.PropertyType)).OrderBy(p => p.Name))
+        {
+          var fieldNode = entityNode.Nodes.Add(browsableProperty.Name);
+          if (typeof (IEntityCore).IsAssignableFrom(browsableProperty.PropertyType))
+          {
+            fieldNode.ImageIndex = 3;
+            fieldNode.Tag = browsableProperty;
+          }
+          else
+            fieldNode.ImageIndex = 1;
+        }
 
-				foreach (var browsableProperty in ListBindingHelper.GetListItemProperties(entityType).Cast<PropertyDescriptor>().
-					Where(p => typeof (IList).IsAssignableFrom(p.PropertyType)).OrderBy(p => p.Name))
-				{
-					var fieldNode = entityNode.Nodes.Add(browsableProperty.Name);
-					fieldNode.Tag = browsableProperty;
-					fieldNode.ImageIndex = 2;
-				}
-			}
-		}
+        foreach (var entityTypeProperty in GetPropertiesOfTypeEntity(entityType))
+        {
+          var fieldNode = entityNode.Nodes.Add(entityTypeProperty.Name);
 
-		public IEnumerable<Type> GetEntitiesTypes()
-		{
-			if (_baseType != null)
-				return MetaDataHelper.GetDescendance(_baseType);
-			return _entityAssembly == null ? MetaDataHelper.GetAllLoadedDescendance(typeof (IEntityCore)) : typeof(IEntityCore).GetAssignable(_entityAssembly.GetExportedTypes());
-		}
+          fieldNode.ImageIndex = 3;
+          fieldNode.Tag = entityTypeProperty;
+        }
 
-		private void treeViewEntities_ItemDrag(object sender, ItemDragEventArgs e)
-		{
-			DoDragDrop(((TreeNode)e.Item).Text, DragDropEffects.All);
-		}
+        foreach (var browsableProperty in ListBindingHelper.GetListItemProperties(entityType).Cast<PropertyDescriptor>().
+          Where(p => typeof (IList).IsAssignableFrom(p.PropertyType)).OrderBy(p => p.Name))
+        {
+          var fieldNode = entityNode.Nodes.Add(browsableProperty.Name);
+          fieldNode.Tag = browsableProperty;
+          fieldNode.ImageIndex = 2;
+        }
+      }
+    }
 
-		private void treeViewEntities_Click(object sender, EventArgs e)
-		{
-		}
+    /// <summary>
+    /// Gets the properties of type entity since for selfservicing these properties are not browsable so they need to be handled as a special case.
+    /// </summary>
+    /// <param name="type">The type.</param>
+    /// <returns></returns>
+    public static IEnumerable<PropertyDescriptor> GetPropertiesOfTypeEntity(Type type)
+    {
+      return from propertyDescriptor in TypeDescriptor.GetProperties(type, null).Cast<PropertyDescriptor>()
+             where typeof (IEntity).IsAssignableFrom(propertyDescriptor.PropertyType)
+             select propertyDescriptor;
+    }
 
-		private void treeViewEntities_AfterSelect(object sender, TreeViewEventArgs e)
-		{
-			var prop = treeViewEntities.SelectedNode.Tag as PropertyDescriptor;
-			if (prop != null)
-			{
-				var typeParameter = MetaDataHelper.GetTypeParameterOfGenericType(prop.PropertyType);
-				if (typeParameter != null)
+    public IEnumerable<Type> GetEntitiesTypes()
+    {
+      if (_baseType != null)
+        return MetaDataHelper.GetDescendance(_baseType);
+      return _entityAssembly == null ? MetaDataHelper.GetAllLoadedDescendance(typeof (IEntityCore)) : typeof (IEntityCore).GetAssignable(_entityAssembly.GetExportedTypes());
+    }
 
-					if (treeViewEntities.Nodes.ContainsKey(typeParameter.Name))
-					{
-						treeViewEntities.SelectedNode = treeViewEntities.Nodes[typeParameter.Name];
-						treeViewEntities.SelectedNode.Expand();
-					}
-			}
-			toolStripStatusLabelSelected.Text = treeViewEntities.SelectedNode.Text;
-		}
-	}
+    private void treeViewEntities_ItemDrag(object sender, ItemDragEventArgs e)
+    {
+      DoDragDrop(((TreeNode) e.Item).Text, DragDropEffects.All);
+    }
+
+    private void treeViewEntities_Click(object sender, EventArgs e)
+    {
+    }
+
+    private void treeViewEntities_AfterSelect(object sender, TreeViewEventArgs e)
+    {
+      var prop = treeViewEntities.SelectedNode.Tag as PropertyDescriptor;
+      if (prop != null)
+      {
+        var typeParameter = ListBindingHelper.GetListItemType(prop.PropertyType);
+        //if (typeof(IEntityCore).IsAssignableFrom(prop.PropertyType))
+        //  typeParameter = prop.PropertyType;
+        //if (typeParameter == null)
+        //  typeParameter = MetaDataHelper.GetTypeParameterOfGenericType(prop.PropertyType);
+        if (typeParameter != null)
+          if (treeViewEntities.Nodes.ContainsKey(typeParameter.Name))
+          {
+            treeViewEntities.SelectedNode = treeViewEntities.Nodes[typeParameter.Name];
+            treeViewEntities.SelectedNode.Expand();
+          }
+      }
+      toolStripStatusLabelSelected.Text = treeViewEntities.SelectedNode.Text;
+    }
+  }
 }
