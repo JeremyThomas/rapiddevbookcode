574712e71fe7312b6d4c8df1b92f1cb9455d7e1d
Jeremy Thomas <SND\JezzaT_cp>
2009-12-21 08:43:37 +0000

    First check in of a static LLBL Data Context Driver - just the UniversalStaticDriver from the DataContextDriverDemo unchanged

---
+++ AW.LLBLGen.DataContextDriver/DevDeploy.bat
@@ -0,0 +1,18 @@
+rem
+rem  You can simplify development by updating this batch file and then calling it from the 
+rem  project's post-build event.
+rem
+rem  It copies the output .DLL (and .PDB) to LINQPad's drivers folder, so that LINQPad
+rem  picks up the drivers immediately (without needing to click 'Add Driver').
+rem
+rem  NB: The target directory may not be correct for your computer!
+rem  You can obtain the first part of the directory by running the following query:
+rem
+rem    Path.Combine (
+rem       Environment.GetFolderPath (Environment.SpecialFolder.CommonApplicationData),
+rem       @"LINQPad\Drivers\DataContext\3.5\")   
+rem
+rem  The final part of the directory is the name of the assembly plus its public key token in brackets.
+
+xcopy /i/y AW.LLBLGen.DataContextDriver.dll "C:\Documents and Settings\All Users\Application Data\LINQPad\Drivers\DataContext\3.5\AW.LLBLGen.DataContextDriver (30e1e0e3428e39e5)\"
+xcopy /i/y AW.LLBLGen.DataContextDriver.pdb "C:\Documents and Settings\All Users\Application Data\LINQPad\Drivers\DataContext\3.5\AW.LLBLGen.DataContextDriver (30e1e0e3428e39e5)\"
