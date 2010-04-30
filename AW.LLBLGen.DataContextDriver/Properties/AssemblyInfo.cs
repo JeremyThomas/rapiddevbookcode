89abb8891560218c2962ad4ae6c0c6f0cd5000d9
Jeremy Thomas <SND\JezzaT_cp>
2010-04-30 11:10:56 +0000

    tidied ConnectionDialog

--- AW.LLBLGen.DataContextDriver/Properties/AssemblyInfo.cs
+++ AW.LLBLGen.DataContextDriver/Properties/AssemblyInfo.cs
@@ -39,4 +39,4 @@ using System.Runtime.InteropServices;
 
 // The AssemblyFileVersion appears to users when they import Data Context Drivers.
 
-[assembly: AssemblyFileVersion("1.0.0.5")]
+[assembly: AssemblyFileVersion("1.0.0.6")]
