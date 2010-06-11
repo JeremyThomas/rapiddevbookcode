b3aba18078bafceb8509ad1cc22b1f1692ffe6cf
Jeremy Thomas <jeremy_thomas@hotmail.com>
2010-06-11 07:50:10 +0000

    DataAccessAdapter Factory for LLBLv2.6 LINQPad data context driver

--- AW.Winforms.Helpers/Controls/GridDataEditorT.cs
+++ AW.Winforms.Helpers/Controls/GridDataEditorT.cs
@@ -6,63 +6,63 @@ using JesseJohnston;
 
 namespace AW.Winforms.Helpers.Controls
 {
-  public partial class GridDataEditorT<T> : GridDataEditor
-  {
-    private IEnumerable<T> _supersetG;
+	public partial class GridDataEditorT<T> : GridDataEditor
+	{
+		private IEnumerable<T> _supersetG;
 
-  	protected override Type ItemType
-  	{
+		protected override Type ItemType
+		{
 			get { return typeof(T); }
-  	}
+		}
 
-  	public bool BindEnumerable(IEnumerable<T> enumerable)
-    {
-      return BindEnumerable(enumerable, PageSize);
-    }
+		public bool BindEnumerable(IEnumerable<T> enumerable)
+		{
+			return BindEnumerable(enumerable, PageSize);
+		}
 
-    public bool BindEnumerable(IEnumerable<T> enumerable, ushort pageSize)
-    {
-      bindingSourcePaging.DataSource = CreatePageDataSource(pageSize, enumerable);
-      return GetFirstPage(enumerable);
-    }
+		public bool BindEnumerable(IEnumerable<T> enumerable, ushort pageSize)
+		{
+			bindingSourcePaging.DataSource = CreatePageDataSource(pageSize, enumerable);
+			return GetFirstPage(enumerable);
+		}
 
-    private bool GetFirstPage(IEnumerable<T> enumerable)
-    {
-      if (Paging())
-        enumerable = enumerable.Take(PageSize);
-      var isEnumerable = bindingSourceEnumerable.BindEnumerable(enumerable, EnumerableShouldBeReadonly(enumerable, typeof (T)));
-      if (bindingSourceEnumerable.DataSource is ObjectListView<T>)
-        ((ObjectListView<T>) bindingSourceEnumerable.DataSource).RemovingItem += GridDataEditor_RemovingItem;
-      else
-        SetRemovingItem();
-      return isEnumerable;
-    }
+		private bool GetFirstPage(IEnumerable<T> enumerable)
+		{
+			if (Paging())
+				enumerable = enumerable.Take(PageSize);
+			var isEnumerable = bindingSourceEnumerable.BindEnumerable(enumerable, EnumerableShouldBeReadonly(enumerable, typeof (T)));
+			return isEnumerable;
+		}
 
-    protected IEnumerable<int> CreatePageDataSource(ushort pageSize, IEnumerable<T> enumerable)
-    {
-      PageSize = pageSize;
-      if (pageSize == 0)
-        return Enumerable.Empty<int>();
-      _supersetG = enumerable;
-      return Enumerable.Range(1, GetPageCount());
-    }
+		protected IEnumerable<int> CreatePageDataSource(ushort pageSize, IEnumerable<T> enumerable)
+		{
+			PageSize = pageSize;
+			if (pageSize == 0)
+				return Enumerable.Empty<int>();
+			_supersetG = enumerable;
+			return Enumerable.Range(1, GetPageCount());
+		}
 
-    protected override int SuperSetCount()
-    {
-    	return _supersetG != null ? _supersetG.Count() : 0;
-    }
+		protected override int SuperSetCount()
+		{
+			return _supersetG != null ? _supersetG.Count() : 0;
+		}
 
-  	protected override void BindPage()
-    {
-      if (GetPageIndex() > 0)
-        bindingSourceEnumerable.BindEnumerable(SkipTake(), false);
-      else
-        GetFirstPage(_supersetG);
-    }
+		protected override void BindPage()
+		{
+			if (GetPageIndex() > 0)
+				bindingSourceEnumerable.BindEnumerable(SkipTake(), false);
+			else
+				GetFirstPage(_supersetG);
+			if (bindingSourceEnumerable.DataSource is ObjectListView<T>)
+				((ObjectListView<T>)bindingSourceEnumerable.DataSource).RemovingItem += GridDataEditor_RemovingItem;
+			else
+				SetRemovingItem();
+		}
 
-    private IEnumerable<T> SkipTake()
-    {
-      return _supersetG.SkipTake(GetPageIndex(), PageSize);
-    }
-  }
+		private IEnumerable<T> SkipTake()
+		{
+			return _supersetG.SkipTake(GetPageIndex(), PageSize);
+		}
+	}
 }
