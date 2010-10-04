29f590e35ca6dd8bea512c9110d7f024c50c4f9e
Jeremy Thomas <SND\JezzaT_cp>
2010-10-04 10:54:45 +0000

    DataEditorExtensionsTest now use NUnitFormMSTest

---
+++ VS2010/nunitforms/source/NUnitForms/Testers/DataGridViewTester.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NUnit.Extensions.Forms.Testers
+{
+	public class DataGridViewTester : ControlTester<DataGridView, DataGridViewTester>
+	{
+		public DataGridViewTester()
+		{
+		}
+
+		public DataGridViewTester(string name, string formName) : base(name, formName)
+		{
+		}
+
+		public DataGridViewTester(string name, Form form) : base(name, form)
+		{
+		}
+
+		public DataGridViewTester(string name) : base(name)
+		{
+		}
+
+		public DataGridViewTester(ControlTester<DataGridView, DataGridViewTester> tester, int index) : base(tester, index)
+		{
+		}
+	}
+}
