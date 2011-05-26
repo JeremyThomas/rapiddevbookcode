30fae559286de74586fc7bb8365d4310b61658f9
Jeremy Thomas <SND\JezzaT_cp>
2011-05-26 12:23:28 +0000

    AdditionalNamespaces = new ObservableCollection

---
+++ VS2010/AW.Winforms.Helpers/WPF/StringToTypeConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace AW.Winforms.Helpers.WPF
+{
+	/// <summary>
+	/// Convert a string defining a .NET type into a <see cref="System.Type"/> value.  This code is from
+	/// http://compiledexperience.com/blog/posts/creating-a-dependency-property-of-type-systemtype-in-silverlight
+	/// </summary>
+	public class StringToTypeConverter : TypeConverter
+	{
+		/// <summary />
+		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+		{
+			return sourceType.IsAssignableFrom(typeof (string));
+		}
+
+		/// <summary/>
+		public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+		{
+			var typeName = value as string;
+			return String.IsNullOrEmpty(typeName) ? null : Type.GetType(typeName);
+		}
+	}
+}
