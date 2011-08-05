d340987255577d1d90faf779cfd20f165e9c155f
Jeremy Thomas <jeremy_thomas@hotmail.com>
2011-08-05 12:10:21 +0000

    TearDownContext

--- AW.LLBLGen.DataContextDriver/Properties/AssemblyInfo.cs
+++ AW.LLBLGen.DataContextDriver/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using AW.LLBLGen.DataContextDriver;
 
@@ -11,8 +12,9 @@ using AW.LLBLGen.DataContextDriver;
 [assembly: AssemblyProduct(Constants.AssemblyProduct)]
 [assembly: AssemblyCompany(Constants.AssemblyCompany)]
 [assembly: AssemblyCopyright(Constants.AssemblyCopyright)]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: InternalsVisibleTo("AW.LLBLGen.DataContextDriver.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f1f80be889fd7e"
+                              + "25b6b34615ae89dcdb2f476a23bfb3ce9a7af92c9c754d6693078c2b2be06cd748f600a2d1c46da07260f76d289a97a4138df273f71c404cc2f9d8643613bda39b4d46ea706847732c2c98cb4dd5"
+                              + "d13644dd9c02060dde57ca5683692062534c41c842a577a0e56d2af7dcdbf96cd01b1070a5bfdfc85794cf")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
