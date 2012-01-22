261b5bb69bbec806aa48c4aac06164e45b7401ac
Jeremy Thomas <SND\JezzaT_cp>
2012-01-22 08:09:39 +0000

    Northwind.DAL.Services

---
+++ LLBL Pro v3.1/Northwind/DAL/Services/Factories.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.OleDb;
+using AW.Helper;
+using Northwind.DAL.SqlServer;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+
+namespace Northwind.DAL.Services
+{
+	public static class Factories
+	{
+		public static DataAccessAdapterBase CreateDataAccessAdapter(string oleDbConnectionString)
+		{
+			var oleDbConnectionStringBuilder = new OleDbConnectionStringBuilder(oleDbConnectionString);
+			var connectionStringBuilder = DataHelper.ConvertConnectionStringBuilder(oleDbConnectionStringBuilder, DataHelper.GetDBSourceType(oleDbConnectionString));
+			return new DataAccessAdapter(connectionStringBuilder.ConnectionString);
+		}
+	}
+}
