4a4749c5a6c5dc1848e220623860dacbcf36f2d6
Jeremy Thomas <SND\JezzaT_cp>
2012-01-27 08:26:57 +0000

    DynamicDataSupportClasses

---
+++ LLBL Pro v3.1/DynamicDataSupportClasses/ConstantsEnums.cs
@@ -0,0 +1,55 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////
+// LLBLGen Pro is (c) 2002-2008 Solutions Design. All rights reserved.
+// http://www.llblgen.com
+// The sourcecode for the Dynamic Data Support classes has been made available to LLBLGen Pro licensees
+// so they can modify, update and/or extend it. Distribution of this sourcecode in textual, non-compiled, 
+// non-binary form to non-licensees is prohibited. Distribution of binary compiled versions of this 
+// sourcecode to non-licensees has been granted under the following license.
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+// COPYRIGHTS:
+// Copyright (c)2002-2008 Solutions Design. All rights reserved.
+// http://www.llblgen.com
+// 
+// The Dynamic Data Support classes library sourcecode is released to LLBLGen Pro licensees under the 
+// following license:
+// --------------------------------------------------------------------------------------------
+// 
+// Redistribution and use of the sourcecode in compiled, binary forms, with or without modification, 
+// are permitted provided that the following conditions are met: 
+//
+// 1) Redistributions must reproduce the above copyright notice, this list of 
+//    conditions and the following disclaimer in the documentation and/or other materials 
+//    provided with the distribution. 
+// 2) Redistribution of the sourcecode in textual, non-binary, non-compiled form is prohibited.
+// 
+// THIS SOFTWARE IS PROVIDED BY SOLUTIONS DESIGN ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SOLUTIONS DESIGN OR CONTRIBUTORS BE LIABLE FOR 
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+//
+// The views and conclusions contained in the software and documentation are those of the authors 
+// and should not be interpreted as representing official policies, either expressed or implied, 
+// of Solutions Design. 
+//////////////////////////////////////////////////////////////////////
+// Contributers to the code:
+//		- Frans Bouma [FB]
+//////////////////////////////////////////////////////////////////////
+using System;
+
+namespace SD.LLBLGen.Pro.DynamicDataSupportClasses
+{
+	/// <summary>
+	/// Class for constants
+	/// </summary>
+	public static class Constants
+	{
+		/// <summary>
+		/// The key used to store the datamodel provider with in the Application object.
+		/// </summary>
+		public static readonly string DataModelProviderKey = "__DataModelProvider";
+	}
+}
---
+++ LLBL Pro v3.1/DynamicDataSupportClasses/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿#pragma warning disable 1699
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System;
+
+[assembly: AssemblyTitle("LLBLGen Pro Dynamic Data Support Classes library")]
+[assembly: AssemblyDescription("Classes for Dynamic Data support for LLBLGen Pro")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Solutions Design")]
+[assembly: AssemblyProduct("LLBLGen Pro")]
+[assembly: AssemblyCopyright("Copyright © 2008 Solutions Design")]
+[assembly: AssemblyTrademark("LLBLGen and LLBLGen Pro are trademarks of Solution Design. Dynamic Data is a trademark of Microsoft")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
+
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyName("")]
