24b9ae4bd917bda17972e30d6dc6f124a5838177
Jeremy Thomas <SND\JezzaT_cp>
2012-07-19 10:21:21 +0000

    Merge from 3.1 to 3.5

--- LLBL Pro v3.5/AW.LinqPadExtensions/DevDeployE.bat
+++ LLBL Pro v3.5/AW.LinqPadExtensions/DevDeployE.bat
@@ -2,6 +2,6 @@ rem
 rem  You can simplify development by updating this batch file and then calling it from the 
 rem  project's post-build event.
 rem
-xcopy /i/y *.dll "C:\Users\Jeremy Thomas\Documents\LINQPad Plugins"
-xcopy /i/y *.pdb "C:\Users\Jeremy Thomas\Documents\LINQPad Plugins"
+xcopy /i/y *.dll "%homepath%\Documents\LINQPad Plugins"
+xcopy /i/y *.pdb "%homepath%\Documents\LINQPad Plugins"
 pause
