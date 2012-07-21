588f1ab0da379c2b21a66ca33752e27df2a7ba84
Jeremy Thomas <jeremy_thomas@hotmail.com>
2012-07-21 10:55:43 +0000

    Options to use when a query is run with 'Results to Data Grids' EnumerationExtension DataContextDriver v2.2

---
+++ AW.Winforms.Helpers/WPF/EnumerationExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Markup;
+using AW.Helper;
+
+namespace AW.Winforms.Helpers.WPF
+{
+  public class EnumerationExtension : MarkupExtension
+  {
+    private Type _enumType;
+
+    public EnumerationExtension(Type enumType)
+    {
+      if (enumType == null) throw new ArgumentNullException("enumType");
+      EnumType = enumType;
+    }
+
+    public Type EnumType
+    {
+      get { return _enumType; }
+      private set
+      {
+        if (_enumType == value) return;
+        var enumType = MetaDataHelper.GetCoreType(value);
+        if (enumType.IsEnum == false) throw new ArgumentException("Type must be an Enum.");
+        _enumType = value;
+      }
+    }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+      var enumValues = Enum.GetValues(EnumType);
+      return (from object enumValue in enumValues select new EnumerationMember { Value = enumValue, Description = ((Enum)enumValue).EnumToString() }).ToArray();
+    }
+
+    public class EnumerationMember
+    {
+      public string Description { get; set; }
+      public object Value { get; set; }
+    }
+
+  }
+}
