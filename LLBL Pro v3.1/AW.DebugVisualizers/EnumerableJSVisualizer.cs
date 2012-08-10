413ac3b94aa3f93f534df6ab18483b174a303df0
Jeremy Thomas <SND\JezzaT_cp>
2012-08-10 11:17:28 +0000

    DebugJSVisualizersTest now doesn't throw exceptions

--- LLBL Pro v3.1/AW.DebugVisualizers/EnumerableJSVisualizer.cs
+++ LLBL Pro v3.1/AW.DebugVisualizers/EnumerableJSVisualizer.cs
@@ -2,11 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.IO;
 using System.Linq;
 using System.Linq.Dynamic;
 using System.Runtime.Serialization;
+using System.Xml.Schema;
 using AW.Helper;
 using AW.Winforms.Helpers.DataEditor;
 using Microsoft.VisualStudio.DebuggerVisualizers;
@@ -44,27 +46,46 @@ namespace AW.DebugVisualizers
         throw new NotSupportedException("This debugger does not support modal visualizers");
 
       var s2 = new StreamReader(objectProvider.GetData()).ReadToEnd();
+      var enumerable = JsonStringToEnumerable(s2);
+      if (enumerable != null)
+        _modalService.ShowDialog(FrmDataEditor.CreateDataViewForm(enumerable));
+    }
+
+    private IEnumerable JsonStringToEnumerable(string s2)
+    {
+      var deserializeObject = JsonConvert.DeserializeObject(s2, EnumerableJSVisualizerObjectSource.JsonSerializerSettingsTypeNameHandlingAll);
       DataTable dataTable = null;
-      try
+      IEnumerable enumerable = null;
+      if (deserializeObject is JArray)
       {
-        dataTable = JsonConvert.DeserializeObject<DataTable>(s2, new DataTableConverter());
+        dataTable = CopyToDataTable(deserializeObject as JArray);
       }
-      catch (Exception)
+      else if (deserializeObject is IEnumerable)
       {
-        var jToken = JToken.Parse(s2);
-        if (jToken is JArray)
-          dataTable = CopyToDataTable(jToken as JArray);
-        else
+        enumerable = (IEnumerable) deserializeObject;
+      }
+      else
+      {
+        try
         {
-          ;
+          dataTable = JsonConvert.DeserializeObject<DataTable>(s2, new DataTableConverter());
+        }
+        catch (Exception)
+        {
+          var jToken = JToken.Parse(s2);
+          if (jToken is JArray)
+            dataTable = CopyToDataTable(jToken as JArray);
+          else
+          {
+            ;
+          }
         }
       }
-
-
       if (dataTable != null)
       {
-        _modalService.ShowDialog(FrmDataEditor.CreateDataViewForm(dataTable.DefaultView));
+        enumerable = dataTable.DefaultView;
       }
+      return enumerable;
     }
 
     public DataTable CopyToDataTable(JArray source)
@@ -79,11 +100,21 @@ namespace AW.DebugVisualizers
               select
                 new JPropertyDescriptor(childToken.Name, typeof (object))).Cast<PropertyDescriptor>();
     }
+
+    public object DeserializeJS(Stream serializationStream)
+    {
+      var s2 = new StreamReader(serializationStream).ReadToEnd();
+      var enumerable = JsonStringToEnumerable(s2);
+      if (enumerable != null)
+        return enumerable;
+      return JToken.Parse(s2);
+    }
   }
 
   public class EnumerableJSVisualizerObjectSource : VisualizerObjectSource
   {
-    private static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings {ReferenceLoopHandling = ReferenceLoopHandling.Ignore}; //, TypeNameHandling = TypeNameHandling.All
+    public static readonly JsonSerializerSettings JsonSerializerSettingsTypeNameHandlingAll = new JsonSerializerSettings {ReferenceLoopHandling = ReferenceLoopHandling.Ignore, TypeNameHandling = TypeNameHandling.All}; //
+    public static readonly JsonSerializerSettings JsonSerializerSettingsReferenceLoopHandlingIgnore = new JsonSerializerSettings {ReferenceLoopHandling = ReferenceLoopHandling.Ignore};
 
     #region Overrides of VisualizerObjectSource
 
@@ -109,69 +140,53 @@ namespace AW.DebugVisualizers
       var enumerable = target as IEnumerable;
       if (enumerable != null)
       {
-        var dataView = enumerable as DataView;
-        if (dataView != null)
-        {
-          SerializeJS(outgoingData, dataView.Table);
-          return;
-        }
-        var itemType = MetaDataHelper.GetEnumerableItemType(enumerable);
         var queryable = enumerable as IQueryable;
         if (queryable != null)
           enumerable = queryable.Take(100);
-        var enumerableType = enumerable.GetType();
-        if (itemType.Implements(typeof (ISerializable)) || enumerableType.Implements(typeof (ISerializable)))
-          SerializeJS(outgoingData, enumerable.CopyToDataTable());
-        else
-          SerializeJS(outgoingData, enumerable);
+        SerializeJS(outgoingData, enumerable);
       }
       else if (target is DataTable)
-        SerializeJS(outgoingData, target);
+        SerializeJSPlain(outgoingData, target);
     }
 
     #endregion
 
+    public static void SerializeJS(Stream outgoingData, IEnumerable enumerable)
+    {
+      var dataView = enumerable as DataView;
+      if (dataView != null)
+      {
+        SerializeJS(outgoingData, dataView.Table);
+        return;
+      }
+      var enumerableType = enumerable.GetType();
+      var itemType = MetaDataHelper.GetEnumerableItemType(enumerable);
+      if (itemType.Implements(typeof (ISerializable)) || enumerableType.Implements(typeof (ISerializable)))
+        StringToStream(outgoingData, JsonConvert.SerializeObject(enumerable.CopyToDataTable(), JsonSerializerSettingsReferenceLoopHandlingIgnore));
+      else if (enumerable is DataRelationCollection || enumerableType.FullName.Contains("JesseJohnston.ObjectListView") || itemType.IsAssignableTo(typeof (SettingsProperty))
+               || itemType.IsAssignableTo(typeof (PropertyDescriptor)) || itemType.IsAssignableTo(typeof (DataRow)) || enumerable.GetType().Implements("SD.LLBLGen.Pro.ORMSupportClasses.IEntityFields"))
+        SerializeJSPlain(outgoingData, enumerable);
+      else if (enumerable is XmlSchemaObjectCollection)
+        StringToStream(outgoingData, JsonConvert.SerializeObject(enumerable, JsonSerializerSettingsReferenceLoopHandlingIgnore));
+      else
+        SerializeJS(outgoingData, (object) enumerable);
+    }
+
+    private static void SerializeJSPlain(Stream outgoingData, object enumerable)
+    {
+      StringToStream(outgoingData, JsonConvert.SerializeObject(enumerable));
+    }
+
     public static void SerializeJS(Stream outgoingData, object enumerable)
     {
-      //TypeSerializer.SerializeToStream(enumerable, outgoingData);
-      var s1 = JsonConvert.SerializeObject(enumerable, JsonSerializerSettings);
+      StringToStream(outgoingData, JsonConvert.SerializeObject(enumerable, JsonSerializerSettingsTypeNameHandlingAll));
+    }
+
+    private static void StringToStream(Stream outgoingData, string s1)
+    {
       var writer = new StreamWriter(outgoingData);
       writer.Write(s1);
       writer.Flush();
-    }
-
-    public static JArray DeserializeJS(Stream serializationStream)
-    {
-      var s2 = new StreamReader(serializationStream).ReadToEnd();
-      try
-      {
-        var jToken = JToken.Parse(s2);
-        if (jToken is JArray)
-        {
-          try
-          {
-          var deserializeObject = JsonConvert.DeserializeObject<DataTable>(s2, new DataTableConverter());
-          }
-          catch (Exception)
-          {
-          }
-          return jToken as JArray;
-        }
-        var jsonObject = JArray.Parse(s2);
-        var x1 = JsonConvert.DeserializeObject(s2);
-        var deserializeObject1 = JsonConvert.DeserializeObject<DataTable>(s2, new DataTableConverter());
-        return jsonObject;
-      }
-      catch (Exception e)
-      {
-        var jsonObject = JObject.Parse(s2);
-        var x = JsonConvert.DeserializeObject(s2);
-      }
-
-      //if (jsonObject is JContainer)
-      //  return jsonObject as JContainer;
-      // return JsonConvert.DeserializeObject(s2, JsonSerializerSettings);
-      return null;
     }
   }
 }
