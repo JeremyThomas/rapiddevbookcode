2d0daa6b5455b25d83ab29e87ab80e15213bed2f
Jeremy Thomas <jeremy_thomas@hotmail.com>
2012-08-28 14:42:22 +0000

    ListIISApps

---
+++ WebSites/ListIISApps/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Microsoft.Web.Administration;
+
+namespace ListIISApps.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      ViewBag.Message = "List Of Applications on IIS";
+      var mgr = new ServerManager();
+
+      var x = mgr.Sites.SelectMany(s => s.Applications);
+      return View(x);
+    }
+  }
+}
---
+++ WebSites/ListIISApps/Models/ServerManagerVM.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Web.Administration;
+using Microsoft.Web.Administration.Interop;
+
+namespace ListIISApps.Models
+{
+  [DebuggerDisplay("Path = {Path}")]
+  public sealed class Application : ConfigurationElement
+  {
+    private VirtualDirectoryDefaults _virtualDirectoryDefaults;
+    private VirtualDirectoryCollection _virtualDirectories;
+    private Site _site;
+    private ServerManager _owner;
+   
+
+    public string ApplicationPoolName
+    {
+      get
+      {
+        return (string)this["applicationPool"];
+      }
+      set
+      {
+        this["applicationPool"] = (object)value;
+      }
+    }
+
+    public string EnabledProtocols
+    {
+      get
+      {
+        return (string)this["enabledProtocols"];
+      }
+      set
+      {
+        this["enabledProtocols"] = (object)value;
+      }
+    }
+
+    public string Path
+    {
+      get
+      {
+
+        return null;
+      }
+      set
+      {
+ 
+      }
+    }
+
+
+    internal Site Site
+    {
+      get
+      {
+        return this._site;
+      }
+    }
+
+    public VirtualDirectoryCollection VirtualDirectories
+    {
+      get
+      {
+        if (this._virtualDirectories == null)
+        {
+          this._virtualDirectories = (VirtualDirectoryCollection)this.GetCollection(typeof(VirtualDirectoryCollection));
+        }
+        return this._virtualDirectories;
+      }
+    }
+
+    public VirtualDirectoryDefaults VirtualDirectoryDefaults
+    {
+      get
+      {
+
+        return this._virtualDirectoryDefaults;
+      }
+    }
+
+    internal Application(ServerManager owner, Site site)
+    {
+      this._site = site;
+      this._owner = owner;
+    }
+
+    public Configuration GetWebConfiguration()
+    {
+      return this._owner.GetWebConfiguration(this.Site.Name, this.Path);
+    }
+
+
+  }
+
+}
