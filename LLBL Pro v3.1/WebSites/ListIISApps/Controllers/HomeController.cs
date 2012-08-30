bcf16024914e1d9066837a3a8548816cc8f062a9
Jeremy Thomas <SND\JezzaT_cp>
2012-08-30 08:49:20 +0000

    ServerManagerVM with appsettings

--- LLBL Pro v3.1/WebSites/ListIISApps/Controllers/HomeController.cs
+++ LLBL Pro v3.1/WebSites/ListIISApps/Controllers/HomeController.cs
@@ -1,17 +1,19 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
+using ListIISApps.Models;
 using Microsoft.Web.Administration;
 
 namespace ListIISApps.Controllers
 {
+  
   public class HomeController : Controller
   {
     public ActionResult Index()
     {
       ViewBag.Message = "List Of Applications on IIS";
-      var mgr = new ServerManager();
-
-      var x = mgr.Sites.SelectMany(s => s.Applications);
+      var x = ServerManagerVM.GetServerManagerVms();
       return View(x);
     }
   }
