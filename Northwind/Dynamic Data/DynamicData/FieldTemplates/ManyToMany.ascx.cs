0f0eb765acc76557f6e6aed371ecf19b1301d7f6
Jeremy Thomas <jeremy_thomas@hotmail.com>
2012-10-25 11:40:32 +0000

    ProductViewDto

--- Northwind/Dynamic Data/DynamicData/FieldTemplates/ManyToMany.ascx.cs
+++ Northwind/Dynamic Data/DynamicData/FieldTemplates/ManyToMany.ascx.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
 using System.Web.DynamicData;
 using System.Web.UI;
+using AW.Helper.LLBL;
 using Northwind.DAL.SqlServer;
 using SD.LLBLGen.Pro.ORMSupportClasses;
 
@@ -21,38 +23,18 @@ namespace Dynamic_Data
         entity = rowDescriptor.GetPropertyOwner(null) as IEntity2;
       }
       else
-      {
         entity = Row as IEntity2;
-      }
 
       //Get the collection and make sure it's loaded
       if (entity != null)
       {
         var entityCollection = Column.EntityTypeProperty.GetValue(entity, null) as IEntityCollection2;
         if (entityCollection == null)
-        {
           throw new InvalidOperationException(String.Format("The ManyToMany template does not support the collection type of the '{0}' column on the '{1}' table.", Column.Name, Table.Name));
-        }
-        // var llblGenProDataModelProvider = HttpContext.Current.Application[Constants.DataModelProviderKey] as LLBLGenProDataModelProvider;
-        // var x = llblGenProDataModelProvider.CreateContext();
-        var dataAccessAdapter = new DataAccessAdapter();
-        IRelationPredicateBucket bucket = new RelationPredicateBucket();
-        bucket.Relations.AddRange(entity.GetRelationsForFieldOfType(Column.EntityTypeProperty.Name));
-        foreach (var primaryKeyColumn in Column.Table.PrimaryKeyColumns)
-        {
-          var entityField2 = entity.Fields[primaryKeyColumn.Name];
-          bucket.PredicateExpression.Add(new FieldCompareValuePredicate(entityField2, null, ComparisonOperator.Equal, entityField2.CurrentValue, entity.LLBLGenProEntityName + "__"));
-        }
 
-        //(new System.Collections.Generic.Mscorlib_CollectionDebugView<System.Web.DynamicData.MetaColumn>(((System.Web.DynamicData.MetaChildrenColumn)(((System.Web.DynamicData.MetaChildrenColumn)(Column)).ColumnInOtherTable)).ChildTable.PrimaryKeyColumns)).Items[0].DisplayName
-        //var y = Column.Table.ForeignKeyColumnsNames;
-        //var x = (((System.Web.DynamicData.MetaChildrenColumn) (Column)).ColumnInOtherTable).Table.ForeignKeyColumnsNames;
-        //var entityField2 = entity.Fields[x];
-        //bucket.PredicateExpression.Add(new FieldCompareValuePredicate(entityField2, null, ComparisonOperator.Equal, entityField2.CurrentValue, "CustomerEntity__"));
-        dataAccessAdapter.FetchEntityCollection(entityCollection, bucket);
-        //if (!entityCollection.IsLoaded) {
-        //    entityCollection.Load();
-        // }
+        var bucket = EntityHelper.GetRelationInfo(entity, Column.EntityTypeProperty.Name, Column.Table.PrimaryKeyColumns.Select(primaryKeyColumn => primaryKeyColumn.Name));
+        using (var dataAccessAdapter = new DataAccessAdapter())   
+          dataAccessAdapter.FetchEntityCollection(entityCollection, bucket);
 
         // Bind the repeater to the list of children entities
         Repeater1.DataSource = entityCollection;
