f15cab771736c6d49cd1d89144c65c0762f1e1ea
Jeremy Thomas <SND\JezzaT_cp>
2013-10-28 10:22:33 +0000

    %USERPROFILE%\Documents

--- LLBL Pro v3.5/AW.DebugVisualizers/DebugDeploy.bat
+++ LLBL Pro v3.5/AW.DebugVisualizers/DebugDeploy.bat
@@ -1,8 +1,23 @@
-xcopy /y  bin\debug\AW.DebugVisualizers.* "%userprofile%\My Documents\Visual Studio 2012\Visualizers"
-xcopy /y  bin\debug\AW.Helper.* "%userprofile%\My Documents\Visual Studio 2012\Visualizers"
-xcopy /y  bin\debug\AW.Winforms.Helpers.* "%userprofile%\My Documents\Visual Studio 2012\Visualizers"
-xcopy /y  bin\debug\Dynamic.* "%userprofile%\My Documents\Visual Studio 2012\Visualizers"
-xcopy /y  bin\debug\ObjectListView.* "%userprofile%\My Documents\Visual Studio 2012\Visualizers"
-xcopy /y  bin\debug\sliver.Windows.Forms.StateBrowser.* "%userprofile%\My Documents\Visual Studio 2012\Visualizers"
-xcopy /y  bin\debug\Newtonsoft.Json.dll "%userprofile%\My Documents\Visual Studio 2012\Visualizers"
+@echo off
+set VisualizersDir= %USERPROFILE%\My Documents\Visual Studio 2012\Visualizers
+IF EXIST %VisualizersDir% (
+  echo using 1 %VisualizersDir%
+) else (
+    echo %VisualizersDir% not found 1
+    set VisualizersDir= "%USERPROFILE%\Documents\Visual Studio 2012\Visualizers" 
+    IF EXIST %VisualizersDir% (
+      echo using 2 %VisualizersDir%
+      ) else (
+      echo %VisualizersDir% not found 2
+      goto:eof
+    )
+ )
+
+xcopy /y  bin\debug\AW.DebugVisualizers.* %VisualizersDir%
+xcopy /y  bin\debug\AW.Helper.* %VisualizersDir%
+xcopy /y  bin\debug\AW.Winforms.Helpers.* %VisualizersDir%
+xcopy /y  bin\debug\Dynamic.* %VisualizersDir%
+xcopy /y  bin\debug\ObjectListView.* %VisualizersDir%
+xcopy /y  bin\debug\sliver.Windows.Forms.StateBrowser.* %VisualizersDir%
+xcopy /y  bin\debug\Newtonsoft.Json.dll %VisualizersDir%
 pause
