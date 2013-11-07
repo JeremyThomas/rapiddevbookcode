e9acad7208e9eed8a44c5e9f8db977c6c044876b
Jeremy Thomas <SND\JezzaT_cp>
2013-11-07 11:44:27 +0000

    DebugVisualizers v2.1

--- LLBL Pro v3.5/AW.DebugVisualizers/Properties/AssemblyInfo.cs
+++ LLBL Pro v3.5/AW.DebugVisualizers/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 [assembly: AssemblyTitle("AW.DebugVisualizers")]
-[assembly: AssemblyDescription("Enumerable Debugger Visualizer")]
-[assembly: AssemblyProduct("Enumerable Debugger Visualizer")]
-[assembly: AssemblyCopyright("Copyright © Jeremy Thomas 2012")]
+[assembly: AssemblyDescription("Enumerable Debugger Visualizer for VS2012")]
+[assembly: AssemblyProduct("Enumerable Debugger Visualizer for VS2012")]
+[assembly: AssemblyCopyright("Copyright © Jeremy Thomas 2013")]
 [assembly: AssemblyCompany("Jeremy Thomas")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.*")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.*")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
