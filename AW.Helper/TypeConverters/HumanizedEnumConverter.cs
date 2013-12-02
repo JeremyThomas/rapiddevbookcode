2879f492119295b6741423224738a2632c51f984
Jeremy Thomas <jeremy_thomas@hotmail.com>
2013-12-02 11:11:19 +0000

    GeneralHelper.EnumsGetValuesPlusUndefined

--- AW.Helper/TypeConverters/HumanizedEnumConverter.cs
+++ AW.Helper/TypeConverters/HumanizedEnumConverter.cs
@@ -6,13 +6,18 @@ using Humanizer;
 namespace AW.Helper.TypeConverters
 {
   /// <summary>
-  ///   An Enum Converter that converts to/from strings using Humanizer
+  ///   An Enum Converter that converts to/from strings using Humanizer.
   /// </summary>
   [Description("An Enum Converter that converts to/from strings using Humanizer")]
   public class HumanizedEnumConverter : EnumConverter
   {
     private readonly Type _coreEnumType;
     private readonly bool _isNullable;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HumanizedEnumConverter"/> class.
+    /// </summary>
+    /// <param name="type">A <see cref="T:System.Type" /> that represents the type of enumeration to associate with this enumeration converter.</param>
     public HumanizedEnumConverter(Type type) : base(type)
     {
       _coreEnumType = MetaDataHelper.GetCoreType(type);
@@ -27,34 +32,48 @@ namespace AW.Helper.TypeConverters
     {
       var coreType = MetaDataHelper.GetCoreType(enumType);
       GeneralHelper.CheckIsEnum(coreType);
-      if (!(TypeDescriptor.GetConverter(enumType) is HumanizedEnumConverter))
+      if (!(TypeDescriptor.GetConverter(enumType) is HumanizedEnumConverter)) //perhaps could just check != EnumConverter
         TypeDescriptor.AddAttributes(enumType, new TypeConverterAttribute(typeof (HumanizedEnumConverter)));
-      if (coreType != enumType)
-      {
-        if (!(TypeDescriptor.GetConverter(coreType) is HumanizedEnumConverter))
-          TypeDescriptor.AddAttributes(coreType, new TypeConverterAttribute(typeof(HumanizedEnumConverter))); 
-      }
+      if (coreType == enumType) return;
+      if (!(TypeDescriptor.GetConverter(coreType) is HumanizedEnumConverter))
+        TypeDescriptor.AddAttributes(coreType, new TypeConverterAttribute(typeof (HumanizedEnumConverter)));
     }
 
+    /// <summary>
+    /// Converts the given value object to the specified destination type.
+    /// When the converting to string and the enum is not defined then return empty string rather than the number
+    /// </summary>
+    /// <param name="context">An <see cref="T:System.ComponentModel.ITypeDescriptorContext" /> that provides a format context.</param>
+    /// <param name="culture">An optional <see cref="T:System.Globalization.CultureInfo" />. If not supplied, the current culture is assumed.</param>
+    /// <param name="value">The <see cref="T:System.Object" /> to convert.</param>
+    /// <param name="destinationType">The <see cref="T:System.Type" /> to convert the value to.</param>
+    /// <returns>
+    /// An <see cref="T:System.Object" /> that represents the converted <paramref name="value" />.
+    /// </returns>
     public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
     {
       if (destinationType == typeof (string) && value != null)
-          return Enum.IsDefined(_coreEnumType, value) ? ((Enum)value).Humanize() : string.Empty;
+        return Enum.IsDefined(_coreEnumType, value) ? ((Enum) value).Humanize() : string.Empty;
       return base.ConvertTo(context, culture, value, destinationType);
     }
 
+    /// <summary>
+    /// Converts the specified value object to an enumeration object.
+    /// </summary>
+    /// <param name="context">An <see cref="T:System.ComponentModel.ITypeDescriptorContext" /> that provides a format context.</param>
+    /// <param name="culture">An optional <see cref="T:System.Globalization.CultureInfo" />. If not supplied, the current culture is assumed.</param>
+    /// <param name="value">The <see cref="T:System.Object" /> to convert.</param>
+    /// <returns>
+    /// An <see cref="T:System.Object" /> that represents the converted <paramref name="value" />.
+    /// </returns>
     public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
     {
       var s = value as string;
       if (s == null) return base.ConvertFrom(context, culture, value);
-      try
-      {
-        return base.ConvertFrom(context, culture, s);
-      }
-      catch (FormatException)
-      {
-        return GeneralHelper.StringToEnum(s, EnumType);
-      }
+      var enumConversion = GeneralHelper.StringToEnum(s, EnumType, false);
+      if (enumConversion == null && !_isNullable)
+        return base.ConvertFrom(context, culture, s); // To throw an exception
+      return enumConversion;
     }
   }
 }
