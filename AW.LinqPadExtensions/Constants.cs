596469b0b7fdb32cb7212c6f048da6e2f846f439
Jeremy Thomas <jeremy_thomas@hotmail.com>
2014-05-15 08:40:15 +0000

    GetExceptionAndInners

--- AW.LinqPadExtensions/Constants.cs
+++ AW.LinqPadExtensions/Constants.cs
@@ -1,17 +1,17 @@
 ﻿namespace AW.LinqPadExtensions
 {
-	internal static class Constants
-	{
-		public const string Author = "Jeremy Thomas";
-		public const string AssemblyCompany = Author;
-		public const string CopyrightYear = "2013";
-		public const string Name = "AW LINQPad Custom Visualizers" + " (v" + Version + ")";
-		public const string AssemblyCopyright = "Copyright " + CopyrightYear + " " + Author;
-		public const string AssemblyProduct = "AW LINQPad Custom Visualizers";
-		public const string AssemblyDescription = "LINQPad Windows Forms DataGridView Custom Visualizer";
-		public const string MajorVersion = "1";
-		public const string MinorVersion = "1";
-		public const string Version = MajorVersion + "." + MinorVersion;
-		public const string AssemblyVersion = Version + ".0.0";
-	}
+  internal static class Constants
+  {
+    public const string Author = "Jeremy Thomas";
+    public const string AssemblyCompany = Author;
+    public const string CopyrightYear = "2014";
+    public const string Name = "AW LINQPad Custom Visualizers" + " (v" + Version + ")";
+    public const string AssemblyCopyright = "Copyright " + CopyrightYear + " " + Author;
+    public const string AssemblyProduct = "AW LINQPad Custom Visualizers";
+    public const string AssemblyDescription = "LINQPad Windows Forms DisplayInGrid, DisplayHierarchyInTree and Inspect Custom Visualizers";
+    public const string MajorVersion = "2";
+    public const string MinorVersion = "0";
+    public const string Version = MajorVersion + "." + MinorVersion;
+    public const string AssemblyVersion = Version + ".0.0";
+  }
 }
