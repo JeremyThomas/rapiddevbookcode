6f46ac796d7382eb06f7bff16601a92485eb96d3
Jeremy Thomas <jeremy_thomas@hotmail.com>
2014-06-14 09:51:51 +0000

    http://docs.nuget.org/docs/workflows/migrating-to-automatic-package-restore

--- AW.DebugVisualizers.Tests/DebugVisualizersTest.cs
+++ AW.DebugVisualizers.Tests/DebugVisualizersTest.cs
@@ -69,14 +69,19 @@ namespace AW.DebugVisualizers.Tests
     // [ClassCleanup()]
     // public static void MyClassCleanup() { }
     //
-    // Use TestInitialize to run code before running each test 
-    // [TestInitialize()]
-    // public void MyTestInitialize() { }
-    //
-    // Use TestCleanup to run code after each test has run
-    // [TestCleanup()]
-    // public void MyTestCleanup() { }
-    //
+    //Use TestInitialize to run code before running each test
+    [TestInitialize()]
+    public void MyTestInitialize()
+    {
+      Init();
+    }
+
+    //Use TestCleanup to run code after each test has run
+    [TestCleanup()]
+    public void MyTestCleanup()
+    {
+      Verify();
+    }
 
     #endregion
 
