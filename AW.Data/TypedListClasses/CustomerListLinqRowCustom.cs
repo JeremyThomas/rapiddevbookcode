7778288b5c6de67ab8d27cd7a5553520dc7ded53
Jeremy Thomas <jeremy_thomas@hotmail.com>
2014-06-18 23:11:05 +0000

    CustomerListLinqRowCustom

---
+++ AW.Data/TypedListClasses/CustomerListLinqRowCustom.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using AW.Data.EntityClasses;
+
+namespace AW.Data.TypedListClasses
+{
+  public partial class CustomerListLinqRow 
+  {
+    public string AddressType { get; set; }
+
+    public CustomerListLinqRow(string addressLine1, string addressLine2, string city, string addressType, string title,
+                                       string firstName, string middleName, string lastName, string suffix, string emailAddress,
+                                       EmailPromotion emailPromotion, string countryRegionName, string stateProvinceName, int customerID)
+    {
+      AddressLine1 = addressLine1;
+      AddressLine2 = addressLine2;
+     City = city;
+      AddressType = addressType;
+      Title = title;
+      FirstName = firstName;
+      MiddleName = middleName;
+      LastName = lastName;
+      Suffix = suffix;
+      EmailAddress = emailAddress;
+      EmailPromotion = emailPromotion;
+      CountryRegionName = countryRegionName;
+      StateProvinceName = stateProvinceName;
+      CustomerId = customerID;
+    }
+
+
+    public static IQueryable<CustomerListLinqRow> GetCustomerListQuery(IQueryable<IndividualEntity> individuals)
+    {
+      return (from individual in individuals
+              from customerAddress in individual.CustomerAddresses
+              select
+                new CustomerListLinqRow(customerAddress.Address.AddressLine1, customerAddress.Address.AddressLine2,
+                                                customerAddress.Address.City, customerAddress.AddressType.Name,
+                                                individual.Contact.Title, individual.Contact.FirstName,
+                                                individual.Contact.MiddleName, individual.Contact.LastName,
+                                                individual.Contact.Suffix, individual.Contact.EmailAddress,
+                                                individual.Contact.EmailPromotion,
+                                                customerAddress.Address.StateProvince.CountryRegion.Name,
+                                                customerAddress.Address.StateProvince.Name, individual.CustomerID));
+    }
+  }
+}
