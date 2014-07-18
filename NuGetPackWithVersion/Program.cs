c41491b0468e3a1c05869c9921ba0e1e8d0b4368
Jeremy Thomas <jeremy_thomas@hotmail.com>
2014-07-18 11:22:09 +0000

    NuGetPackWithVersion

---
+++ NuGetPackWithVersion/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using SlavaGu.ConsoleAppLauncher;
+
+namespace NuGetPackWithVersion
+{
+  internal class Program
+  {
+    // Define other methods and classes here
+    private static string PackageWithVersion(string nuspecFile)
+    {
+      var nuspecDir = Path.GetDirectoryName(nuspecFile);
+      if (nuspecDir == null) return nuspecFile + " not found";
+      var xmlDocument = new XmlDocument();
+     // xmlDocument.Load(nuspecFile);
+   //   XmlNodeList elementsByTagName = xmlDocument.GetElementsByTagName("package")[0].SelectNodes();
+      var doc = XDocument.Load(nuspecFile);
+      var firstAssemblyName = ((XElement) ((XContainer) ((XContainer) doc.FirstNode).LastNode).FirstNode).Attribute("src").Value;
+      var location = Path.Combine(nuspecDir, firstAssemblyName);
+      var fileVersion = FileVersionInfo.GetVersionInfo(location).FileVersion;
+      //var startInfo = new ProcessStartInfo();
+      //startInfo.WorkingDirectory = nuspecDir;
+      //startInfo.Arguments = "Pack "+@""""+nuspecFile+@""""+" -version "+ FileVersion;
+      //startInfo.FileName = @"C:\Program Files (x86)\NuGet\nuget.exe";
+      //startInfo.UseShellExecute = false;
+      //                  startInfo.CreateNoWindow = true;
+      //                    startInfo.RedirectStandardError = true;
+      //                    startInfo.RedirectStandardOutput = true;
+      //                    startInfo.RedirectStandardInput = true;
+
+      //Process proc = Process.Start(startInfo);
+
+      return ConsoleApp.Run(@"C:\Program Files (x86)\NuGet\nuget.exe",
+        string.Format("Pack \"{0}\" -version {1} -OutputDirectory \"{2}\"", nuspecFile, fileVersion, nuspecDir))
+        .Output.Trim();
+    }
+
+    private static void Main(string[] args)
+    {
+      foreach (var s in args)
+        Console.Write(PackageWithVersion(s));
+      Console.ReadLine();
+    }
+  }
+}
