8f5487695c30242838313b58baae1b57776c813d
Jeremy Thomas <jeremy_thomas@hotmail.com>
2014-08-31 13:10:47 +0000

    LLBLGenProODataService

--- Northwind/Northwind.Win/FrmEntityBrowser.cs
+++ Northwind/Northwind.Win/FrmEntityBrowser.cs
@@ -1,7 +1,12 @@
-﻿using AW.Helper;
+﻿using System;
+using System.Configuration;
+using System.Net;
+using AW.Helper;
 using AW.Winforms.Helpers;
 using Northwind.DAL.Linq;
 using Northwind.DAL.SqlServer;
+//using Northwind.Win.NorthwindODataSR;
+using Northwind.Win.NorthwindODataSR;
 
 namespace Northwind.Win
 {
@@ -10,10 +15,27 @@ namespace Northwind.Win
     public FrmEntityBrowser()
     {
       InitializeComponent();
+      var baseAddress = ConfigurationManager.AppSettings["WcfDataServiceUrl"];
+      var uri = new Uri(baseAddress);// + typeof(LLBLGenProODataService).Name);
+      var llblGenProODataService = new LLBLGenProODataService(uri);
+      //var webClient = new WebClient();
+      //var openRead = webClient.OpenRead(llblGenProODataService.Employee.RequestUri);
+      //  llblGenProODataService.BeginExecute<Northwind.DAL.EntityClasses.EmployeeEntity>(llblGenProODataService.Employee.RequestUri, handleResult, null);
+     // llblGenProODataService.Execute<object>(llblGenProODataService.Employee.RequestUri);
+
+
+      var employee = llblGenProODataService.CreateQuery<object>("Employee");
+      var employeeEntities = employee.Execute();
+      //  var categoryEntities = llblGenProODataService.Category.Execute();
       var dataAccessAdapter = new DataAccessAdapter();
       usrCntrlEntityBrowser1.Initialize(new LinqMetaData(dataAccessAdapter));
       //Done in CommonEntityBase  CacheController.RegisterCache(dataAccessAdapter.ConnectionString, new ResultsetCache()); 
       Text += string.Format(" - {0}", ProfilerHelper.OrmProfilerStatus);
+    }
+
+    private void handleResult(IAsyncResult ar)
+    {
+     // ar.
     }
   }
 }
