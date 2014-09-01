76171b2ee66836bad9c295f6d44d6f0f50ea2886
Jeremy Thomas <jeremy_thomas@hotmail.com>
2014-09-01 05:54:39 +0000

    LLBLGenProODataService

--- Northwind/Northwind.Win/FrmEntityBrowser.Designer.cs
+++ Northwind/Northwind.Win/FrmEntityBrowser.Designer.cs
@@ -30,15 +30,38 @@
     {
       System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmEntityBrowser));
       this.usrCntrlEntityBrowser1 = new AW.Winforms.Helpers.LLBL.UsrCntrlEntityBrowser();
+      this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+      this.toolStripButtonRemote = new System.Windows.Forms.ToolStripButton();
+      this.toolStrip1.SuspendLayout();
       this.SuspendLayout();
       // 
       // usrCntrlEntityBrowser1
       // 
       this.usrCntrlEntityBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
-      this.usrCntrlEntityBrowser1.Location = new System.Drawing.Point(0, 0);
+      this.usrCntrlEntityBrowser1.Location = new System.Drawing.Point(0, 25);
       this.usrCntrlEntityBrowser1.Name = "usrCntrlEntityBrowser1";
-      this.usrCntrlEntityBrowser1.Size = new System.Drawing.Size(643, 579);
+      this.usrCntrlEntityBrowser1.Size = new System.Drawing.Size(643, 554);
       this.usrCntrlEntityBrowser1.TabIndex = 0;
+      // 
+      // toolStrip1
+      // 
+      this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonRemote});
+      this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+      this.toolStrip1.Name = "toolStrip1";
+      this.toolStrip1.Size = new System.Drawing.Size(643, 25);
+      this.toolStrip1.TabIndex = 1;
+      this.toolStrip1.Text = "toolStrip1";
+      // 
+      // toolStripButtonRemote
+      // 
+      this.toolStripButtonRemote.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+      this.toolStripButtonRemote.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonRemote.Image")));
+      this.toolStripButtonRemote.ImageTransparentColor = System.Drawing.Color.Magenta;
+      this.toolStripButtonRemote.Name = "toolStripButtonRemote";
+      this.toolStripButtonRemote.Size = new System.Drawing.Size(52, 22);
+      this.toolStripButtonRemote.Text = "Remote";
+      this.toolStripButtonRemote.Click += new System.EventHandler(this.toolStripButtonRemote_Click);
       // 
       // FrmEntityBrowser
       // 
@@ -46,16 +69,22 @@
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(643, 579);
       this.Controls.Add(this.usrCntrlEntityBrowser1);
+      this.Controls.Add(this.toolStrip1);
       this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
       this.Name = "FrmEntityBrowser";
       this.Text = "Northwind Data Browser";
+      this.toolStrip1.ResumeLayout(false);
+      this.toolStrip1.PerformLayout();
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
     #endregion
 
     private AW.Winforms.Helpers.LLBL.UsrCntrlEntityBrowser usrCntrlEntityBrowser1;
+    private System.Windows.Forms.ToolStrip toolStrip1;
+    private System.Windows.Forms.ToolStripButton toolStripButtonRemote;
   }
 }
 
---
+++ Northwind/Northwind.Win/Service References/NorthwindODataSRSharedTypes/LLBLGenProODataService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Northwind.DAL;
+using Northwind.DAL.FactoryClasses;
+using SD.LLBLGen.Pro.LinqSupportClasses;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+
+namespace Northwind.Win.NorthwindODataSRSharedTypes
+{
+  public partial class LLBLGenProODataService : ILinqMetaData
+  {
+    private readonly ILinqMetaData _linqMetaData;
+
+    public LLBLGenProODataService(Uri serviceRoot, ILinqMetaData linqMetaData) :
+      this(serviceRoot)
+    {
+      _linqMetaData = linqMetaData;
+    }
+
+    #region Implementation of ILinqMetaData
+
+    /// <summary>
+    ///   returns the datasource to use in a Linq query for the entity type specified
+    /// </summary>
+    /// <param name="typeOfEntity">the type of the entity to get the datasource for</param>
+    /// <returns>
+    ///   the requested datasource
+    /// </returns>
+    public IDataSource GetQueryableForEntity(int typeOfEntity)
+    {
+      return _linqMetaData.GetQueryableForEntity(typeOfEntity);
+      //var entityFactory2 = GetFactory(typeOfEntity);
+      //Type x;
+      //if (entityFactory2 == null)
+      //  return null;
+      //else
+      //{
+      //  x = entityFactory2.Create().GetType();
+      //  MetaDataHelper.GetPropertyDescriptors()
+      //}
+      //return null;
+    }
+
+    #endregion
+
+    /// <summary>
+    ///   Gets the factory of the entity with the integer version of AQD.Model.EntityType specified
+    /// </summary>
+    /// <param name="typeOfEntity">The type of entity.</param>
+    /// <returns>factory to use or null if not found</returns>
+    public static IEntityFactory2 GetFactory(int typeOfEntity)
+    {
+      return EntityFactoryFactory.GetFactory((EntityType) typeOfEntity);
+    }
+  }
+}
