fe8ab157395d3a56f463b97bef915e5420c55a4e
Jeremy Thomas <jeremy_thomas@hotmail.com>
2014-09-03 23:51:00 +0000

    NorthwindSvr

--- Northwind/Business/Northwind.Business/NorthwindSvr.cs
+++ Northwind/Business/Northwind.Business/NorthwindSvr.cs
@@ -19,221 +19,173 @@
 using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Data.Entity;
-using System.Data.Objects;
-using System.Data.Services.Client;
 using System.Linq;
 using System.ServiceModel;
-using GH.Common.Framework.Persistence;
-using GH.Common.Framework.Persistence.DataServiceCxt;
-using GH.Common.ServiceLocator;
-using GH.Northwind.Business.Entities;
-using GH.Northwind.EntityFramework;
-using GH.Northwind.Persistence.DataServiceCxt;
-using Northwind.DAL;
 using Northwind.DAL.EntityClasses;
+using Northwind.DAL.HelperClasses;
 using Northwind.DAL.Interfaces;
+using Northwind.DAL.Services;
 
-
-namespace GH.Northwind.Business
+namespace Northwind.Business
 {
-    using ObjectCxtNamespace = Persistence.ObjectCxt;
-    using DbCxtNamespace = Persistence.DbCxt;
-    using DataServiceCxtNamespace = Persistence.DataServiceCxt;
-    using ObjectCxtFrameWkNamespace = Common.Framework.Persistence.ObjectCxt;
-    using DbCxtFrameWkNamespace = Common.Framework.Persistence.DbCxt;
-    using DataServiceCxtFrameWkNamespace = Common.Framework.Persistence.DataServiceCxt;
-
-    [ServiceBehavior(ConcurrencyMode=ConcurrencyMode.Multiple,InstanceContextMode = InstanceContextMode.PerCall)]
-    public class NorthwindSvr : INorthwindSvr
+  [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, InstanceContextMode = InstanceContextMode.PerCall)]
+  public class NorthwindSvr : INorthwindSvr
+  {
+    static NorthwindSvr()
     {
-        static NorthwindSvr()
-        {
-            string useWcfDataService = ConfigurationManager.AppSettings["UseWcfDataService"];
-            if (useWcfDataService.ToLower() == "true")
-            {
-                string dataServiceUrl = ConfigurationSettings.AppSettings["WcfDataServiceUrl"];
-                DataCxt.Cxt = new DataServiceContext(new Uri(dataServiceUrl));
-                ServiceLocator<IPersistence<CustomerEntity>>.RegisterService<CustomerPrst>();
-                ServiceLocator<IPersistence<ProductEntity>>.RegisterService<ProductPrst>();
-                ServiceLocator<IPersistence<OrderEntity>>.RegisterService<OrderPrst>();
-                ServiceLocator<IPersistence<OrderDetailEntity>>.RegisterService<Order_DetailPrst>();
-                ServiceLocator<IPersistence<CategoryEntity>>.RegisterService<CategoryPrst>();
-                ServiceLocator<IPersistence<SupplierEntity>>.RegisterService<SupplierPrst>();
-            }
-            else
-            {
-                if (typeof (NorthwindEntities).IsSubclassOf(typeof (ObjectContext)))
-                {
-                    // Below are commented out since now NorthwindEntities is DbContext; Uncomment them out if NorthwindEntities is ObjectContext; 
-
-                    /*ObjectCxtFrameWkNamespace.DataCxt.Cxt = new NorthwindEntities(); 
-                    ServiceLocator<IPersistence<CustomerEntity>>.RegisterService<ObjectCxtNamespace.CustomerPrst>();
-                    ServiceLocator<IPersistence<ProductEntity>>.RegisterService<ObjectCxtNamespace.ProductPrst>();
-                    ServiceLocator<IPersistence<OrderEntity>>.RegisterService<ObjectCxtNamespace.OrderPrst>();
-                    ServiceLocator<IPersistence<OrderDetailEntity>>.RegisterService<ObjectCxtNamespace.Order_DetailPrst>();
-                    ServiceLocator<IPersistence<Category>>.RegisterService<ObjectCxtNamespace.CategoryPrst>();
-                    ServiceLocator<IPersistence<Supplier>>.RegisterService<ObjectCxtNamespace.SupplierPrst>();
-                     */
-                }
-                else if (typeof (NorthwindEntities).IsSubclassOf(typeof (DbContext)))
-                {
-                    Common.Framework.Persistence.DbCxt.DataCxt.Cxt = new NorthwindEntities();
-                    ServiceLocator<IPersistence<CustomerEntity>>.RegisterService<Persistence.DbCxt.CustomerPrst>();
-                    ServiceLocator<IPersistence<ProductEntity>>.RegisterService<Persistence.DbCxt.ProductPrst>();
-                    ServiceLocator<IPersistence<OrderEntity>>.RegisterService<Persistence.DbCxt.OrderPrst>();
-                    ServiceLocator<IPersistence<OrderDetailEntity>>.RegisterService<Persistence.DbCxt.Order_DetailPrst>();
-                    ServiceLocator<IPersistence<Category>>.RegisterService<Persistence.DbCxt.CategoryPrst>();
-                    ServiceLocator<IPersistence<Supplier>>.RegisterService<Persistence.DbCxt.SupplierPrst>();
-                }
-                else
-                {
-                    throw new NotSupportedException("NorthwindSvr: static Constructor: " + typeof (NorthwindEntities) +
-                                                    " isn't a supported type.");
-                }
-            }
-        }
-
-        public List<CustomerEntity> GetCustomers()
-        {
-            return PersistSvr<CustomerEntity>.GetAll().ToList();
-        }
-
-        public void InsertCustomer(CustomerEntity CustomerEntity, bool commit)
-        {
-            PersistSvr<CustomerEntity>.Insert(CustomerEntity, commit);
-        }
-
-        public void UpdateCustomer(CustomerEntity currentCustomer, bool commit)
-        {
-            PersistSvr<CustomerEntity>.Update(currentCustomer, commit);
-        }
-
-        public void DeleteCustomer(String customerId, bool commit)
-        {
-            IQueryable<CustomerEntity> qCustomer = PersistSvr<CustomerEntity>.GetAll();
-            List<CustomerEntity> cus = (from c in qCustomer where c.CustomerID == customerId select c).ToList();
-            if (cus.Count <= 0)
-                throw new ApplicationException("NorthwindSvr::DeleteCustomer: CustomerEntity with Id " + customerId +
-                                               " doesn't exist.");
-            PersistSvr<CustomerEntity>.Delete(cus[0], commit);
-        }
-
-        public List<OrderEntity> GetOrders()
-        {
-            return PersistSvr<OrderEntity>.GetAll().ToList();
-        }
-
-        public List<OrderDetailEntity> GetOrderDetailForAnOrder(int orderId)
-        {
-            IQueryable<OrderDetailEntity> qOrder_Detail = PersistSvr<OrderDetailEntity>.GetAll();
-            List<OrderDetailEntity> ods = (from od in qOrder_Detail where od.OrderID == orderId select od).ToList();
-            return ods;
-        }
-
-        public List<OrderEntity> GetOrderForACustomer(String customerId)
-        {
-            IQueryable<OrderEntity> qOrder = PersistSvr<OrderEntity>.GetAll();
-            List<OrderEntity> orders = (from o in qOrder where o.CustomerID == customerId select o).ToList();
-            return orders;
-        }
-
-        public void CreateOrder(OrderEntity OrderEntity, OrderDetailEntity[] details)
-        {
-            PersistSvr<OrderEntity>.Insert(OrderEntity, true);
-            foreach (var od in details)
-            {
-                od.OrderID = OrderEntity.OrderID;
-                PersistSvr<OrderDetailEntity>.Insert(od, true);
-            }
-        }
-
-        public void UpdateOrder(OrderEntity currentOrder, OrderDetailEntity[] details, bool commit)
-        {
-            PersistSvr<OrderEntity>.Update(currentOrder, commit);
-            foreach (var od in details)
-            {
-                PersistSvr<OrderDetailEntity>.Update(od, commit);
-            }
-        }
-
-        public void DeleteAnOrderDetailFromAnOrder(int orderId, int productId, bool commit)
-        {
-            IQueryable<OrderDetailEntity> qOrder_Detail = PersistSvr<OrderDetailEntity>.GetAll();
-            List<OrderDetailEntity> ods =
-                (from od in qOrder_Detail where od.ProductID == productId && od.OrderID == orderId select od).ToList();
-            if (ods.Count <= 0)
-                throw new ApplicationException(
-                    "NorthwindSvr::DeleteAnOrderDetailFromAnOrder: OrderEntity detail with OrderEntity id " + orderId.ToString() +
-                    " and ProductEntity id: " + productId.ToString() +
-                    " doesn't exist.");
-            PersistSvr<OrderDetailEntity>.Delete(ods[0], commit);
-        }
-
-        public void DeleteOrder(int orderId, bool commit)
-        {
-            IQueryable<OrderEntity> qOrder = PersistSvr<OrderEntity>.GetAll();
-            List<OrderEntity> orders = (from o in qOrder where o.OrderID == orderId select o).ToList();
-            if (orders.Count <= 0)
-                throw new ApplicationException("NorthwindSvr::DeleteOrder: ProductEntity with Id " + orderId.ToString() +
-                                               " doesn't exist.");
-            // Delete each OrderEntity detail
-            List<OrderDetailEntity> orderDetails = GetOrderDetailForAnOrder(orders[0].OrderID);
-            foreach (var od in orderDetails)
-            {
-                DeleteAnOrderDetailFromAnOrder(orders[0].OrderID, od.ProductID, commit);
-            }
-            PersistSvr<OrderEntity>.Delete(orders[0], commit);
-        }
-
-        public List<ProductEntity> GetProducts()
-        {
-            return PersistSvr<ProductEntity>.GetAll().ToList();
-        }
-
-        public ProductEntity GetProductById(int id)
-        {
-            IQueryable<ProductEntity> pds = PersistSvr<ProductEntity>.SearchBy(p => p.ProductID == id);
-            return pds.Count() == 0 ? null : pds.First();
-        }
-
-        public void InsertProduct(ProductEntity ProductEntity, bool commit)
-        {
-            PersistSvr<ProductEntity>.Insert(ProductEntity, commit);
-        }
-
-        public void UpdateProduct(ProductEntity currentProduct, bool commit)
-        {
-            PersistSvr<ProductEntity>.Update(currentProduct, commit);
-        }
-
-        public void DeleteProduct(int productId, bool commit)
-        {
-            IQueryable<ProductEntity> qProduct = PersistSvr<ProductEntity>.GetAll();
-            List<ProductEntity> pros = (from p in qProduct where p.ProductID == productId select p).ToList();
-            if (pros.Count <= 0)
-                throw new ApplicationException("NorthwindSvr::DeleteProduct: ProductEntity with Id " + productId.ToString() +
-                                               " doesn't exist.");
-            PersistSvr<ProductEntity>.Delete(pros[0], commit);
-        }
-
-        
-        public List<Category> GetProductCategories()
-        {
-            return PersistSvr<Category>.GetAll().ToList();
-        }
-
-
-        public List<SupplierEntity> GetSuppliers()
-        {
-            return PersistSvr<Supplier>.GetAll().ToList();
-        }
-
-        public void Commit()
-        {
-            //In our implementation, any entity's commit will do the same: commit all changes to the database
-            // so PersistSvr<OrderEntity>.Commit() and PersistSvr<ProductEntity>.Commit() will do the same.
-            PersistSvr<OrderEntity>.Commit();
-        }
+      var useWcfDataService = ConfigurationManager.AppSettings["UseWcfDataService"];
+      if (useWcfDataService.ToLower() == "true")
+      {
+        var dataServiceUrl = ConfigurationSettings.AppSettings["WcfDataServiceUrl"];
+        //DataCxt.Cxt = new DataServiceContext(new Uri(dataServiceUrl));
+        // PersistSvr
+      }
+      else
+      {
+        PersistSvr.PersistenceProvider = new NorthwindPersistence();
+      }
     }
+
+    public List<CustomerEntity> GetCustomers()
+    {
+      return PersistSvr.GetAll<CustomerEntity>().ToList();
+    }
+
+    public void InsertCustomer(CustomerEntity customerEntity, bool commit)
+    {
+      PersistSvr.Insert(customerEntity, commit);
+    }
+
+    public void UpdateCustomer(CustomerEntity currentCustomer, bool commit)
+    {
+      PersistSvr.Update(currentCustomer, commit);
+    }
+
+    public void DeleteCustomer(String customerId, bool commit)
+    {
+      var qCustomer = PersistSvr.GetAll<CustomerEntity>();
+      var cus = (from c in qCustomer where c.CustomerId == customerId select c).ToList();
+      if (cus.Count <= 0)
+        throw new ApplicationException("NorthwindSvr::DeleteCustomer: CustomerEntity with Id " + customerId +
+                                       " doesn't exist.");
+      PersistSvr.Delete(cus[0], commit);
+    }
+
+    public List<OrderEntity> GetOrders()
+    {
+      return PersistSvr.GetAll<OrderEntity>().ToList();
+    }
+
+    public List<OrderDetailEntity> GetOrderDetailForAnOrder(int orderId)
+    {
+      var qOrderDetail = PersistSvr.GetAll<OrderDetailEntity>();
+      var ods = (from od in qOrderDetail where od.OrderId == orderId select od).ToList();
+      return ods;
+    }
+
+    public List<OrderEntity> GetOrderForACustomer(String customerId)
+    {
+      var qOrder = PersistSvr.GetAll<OrderEntity>();
+      var orders = (from o in qOrder where o.CustomerId == customerId select o).ToList();
+      return orders;
+    }
+
+    public void CreateOrder(OrderEntity orderEntity, OrderDetailEntity[] details)
+    {
+      PersistSvr.Insert(orderEntity, true);
+      foreach (var od in details)
+      {
+        od.OrderId = orderEntity.OrderId;
+        PersistSvr.Insert(od, true);
+      }
+    }
+
+    public void UpdateOrder(OrderEntity currentOrder, OrderDetailEntity[] details, bool commit)
+    {
+      PersistSvr.Update(currentOrder, commit);
+      foreach (var od in details)
+      {
+        PersistSvr.Update(od, commit);
+      }
+    }
+
+    public void DeleteAnOrderDetailFromAnOrder(int orderId, int productId, bool commit)
+    {
+      var qOrderDetail = PersistSvr.GetAll<OrderDetailEntity>();
+      var ods =
+        (from od in qOrderDetail where od.ProductId == productId && od.OrderId == orderId select od).ToList();
+      if (ods.Count <= 0)
+        throw new ApplicationException(
+          "NorthwindSvr::DeleteAnOrderDetailFromAnOrder: OrderEntity detail with OrderEntity id " + orderId +
+          " and ProductEntity id: " + productId +
+          " doesn't exist.");
+      PersistSvr.Delete(ods[0], commit);
+    }
+
+    public void DeleteOrder(int orderId, bool commit)
+    {
+      var qOrder = PersistSvr.GetAll<OrderEntity>();
+      var orders = (from o in qOrder where o.OrderId == orderId select o).ToList();
+      if (orders.Count <= 0)
+        throw new ApplicationException("NorthwindSvr::DeleteOrder: ProductEntity with Id " + orderId +
+                                       " doesn't exist.");
+      // Delete each OrderEntity detail
+      var orderDetails = GetOrderDetailForAnOrder(orders[0].OrderId);
+      foreach (var od in orderDetails)
+      {
+        DeleteAnOrderDetailFromAnOrder(orders[0].OrderId, od.ProductId, commit);
+      }
+      PersistSvr.Delete(orders[0], commit);
+    }
+
+    public List<ProductEntity> GetProducts()
+    {
+      return PersistSvr.GetAll<ProductEntity>().ToList();
+    }
+
+    public ProductEntity GetProductById(int id)
+    {
+      var pds = PersistSvr.GetAll<ProductEntity>().Where(p => p.ProductId == id);
+      return pds.Count() == 0 ? null : pds.First();
+    }
+
+    public void InsertProduct(ProductEntity productEntity, bool commit)
+    {
+      PersistSvr.Insert(productEntity, commit);
+    }
+
+    public void UpdateProduct(ProductEntity currentProduct, bool commit)
+    {
+      PersistSvr.Update(currentProduct, commit);
+    }
+
+    public void DeleteProduct(int productId, bool commit)
+    {
+      var qProduct = PersistSvr.GetAll<ProductEntity>();
+      var pros = (from p in qProduct where p.ProductId == productId select p).ToList();
+      if (pros.Count <= 0)
+        throw new ApplicationException("NorthwindSvr::DeleteProduct: ProductEntity with Id " + productId +
+                                       " doesn't exist.");
+      PersistSvr.Delete(pros[0], commit);
+    }
+
+
+    public List<CategoryEntity> GetProductCategories()
+    {
+      return PersistSvr.GetAll<CategoryEntity>().ToList();
+    }
+
+
+    public List<SupplierEntity> GetSuppliers()
+    {
+      return PersistSvr.GetAll<SupplierEntity>().ToList();
+    }
+
+    public void Commit()
+    {
+      //In our implementation, any entity's commit will do the same: commit all changes to the database
+      // so PersistSvr<OrderEntity>.Commit() and PersistSvr<ProductEntity>.Commit() will do the same.
+      PersistSvr.Commit();
+    }
+  }
 }
---
+++ Northwind/DAL/HelperClasses/PersistSvr.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Copyright © 2012
+ * This code is for the codeproject article "A N-Tier Architecture Sample with ASP.NET MVC3, WCF and Entity Framework" at  
+ * http://www.codeproject.com/Articles/434282/A-N-Tier-Architecture-Sample-with-ASP.NET-MVC3-WCF-and-Entity-Framework. 
+ * Permission to use, copy or modify this software freely is hereby granted, 
+ * provided that this copyright notice appears in the orginal or modified copies. 
+ * 
+ * This code isn't guaranteed to work correctly; it is your own responsibility for 
+ * any result from using this code. 
+ *                           
+ * @description
+ * 
+ * @author  
+ * @version July 18, 2012
+ * @see
+ * @since
+ */
+
+using System;
+using System.Linq;
+using Northwind.DAL.Interfaces;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+
+namespace Northwind.DAL.HelperClasses
+{
+  public static class PersistSvr
+  {
+    private static IPersistence _provider;
+
+    public static Func<IPersistence> ProviderImplementation;
+
+    public static IPersistence PersistenceProvider
+    {
+      get
+      {
+        if (_provider == null) _provider = ProviderImplementation();
+        return _provider;
+      }
+      set { _provider = value; }
+    }
+
+    public static void Insert(IEntity2 entity, bool commit)
+    {
+      PersistenceProvider.Save(entity, commit);
+    }
+
+    public static void Update(IEntity2 entity, bool commit)
+    {
+      PersistenceProvider.Save(entity, commit);
+    }
+
+    public static void Delete(IEntity2 entity, bool commit)
+    {
+      PersistenceProvider.Delete(entity, commit);
+    }
+
+    public static void Commit()
+    {
+      PersistenceProvider.Commit();
+    }
+
+
+    public static IQueryable<T> GetAll<T>() where T : class
+    {
+      return PersistenceProvider.GetQueryableForEntity<T>();
+    }
+  }
+}
