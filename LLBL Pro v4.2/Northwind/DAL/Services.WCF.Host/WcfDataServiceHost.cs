324c0093e56d99c1ee1f03ebea4a15af6277c8f1
Jeremy Thomas <SND\JezzaT_cp>
2014-09-08 09:54:41 +0000

    WcfServiceManager shared

--- LLBL Pro v4.2/Northwind/DAL/Services.WCF.Host/WcfDataServiceHost.cs
+++ LLBL Pro v4.2/Northwind/DAL/Services.WCF.Host/WcfDataServiceHost.cs
@@ -1,8 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Data.Services;
 using System.ServiceModel;
-using System.ServiceModel.Description;
 using AW.Helper;
 
 namespace Northwind.DAL.Services.WCF.Host
@@ -17,39 +17,22 @@ namespace Northwind.DAL.Services.WCF.Host
     ///   a static global
     ///   variable. In webapplication, you're working multi-threaded and you can't use this method.
     /// </summary>
-    private static ServiceHost _serviceHost;
-
-    public static ServiceHost CreateDataServiceHost(Type serviceType, string baseAddress)
-    {
-      var fullBaseAddress = string.Concat(baseAddress, serviceType.Name);
-      var host = new DataServiceHost(serviceType, new[] {new Uri(fullBaseAddress)});
-      WcfUtility.DoIncludeExceptionDetailInFaults(host);
-      return host;
-    }
+    private static List<ServiceHost> _serviceHosts;
 
     /// <summary>
     ///   Starts the service.
     /// </summary>
     /// <returns>true if service was successfully opened, false otherwise</returns>
-    internal static ServiceHost StartService()
+    internal static IEnumerable<ServiceHost> StartService()
     {
-      if (_serviceHost == null)
-      {
-        var baseAddress = ConfigurationManager.AppSettings["WcfDataServiceUrl"];
-        //Instantiate new ServiceHost 
-        _serviceHost = CreateDataServiceHost(typeof(NorthwindODataService), baseAddress);
-        _serviceHost.Open();
-      }
+      var baseAddress = ConfigurationManager.AppSettings["WcfDataServiceUrl"];
+      if (_serviceHosts == null)
+        //Instantiate new ServiceHosts 
+        _serviceHosts = new List<ServiceHost>() {CreateDataServiceHost(typeof (NorthwindODataService), baseAddress)};
+      WcfUtility.Open(_serviceHosts);
 
-      if (_serviceHost.State != CommunicationState.Opened && _serviceHost.State != CommunicationState.Opening)
-      {
-        //Open myServiceHost
-        _serviceHost.Open();
-      }
-
-      return _serviceHost;
+      return _serviceHosts;
     }
-
 
     /// <summary>
     ///   Stops the service.
@@ -57,27 +40,27 @@ namespace Northwind.DAL.Services.WCF.Host
     /// <returns>true if the service was successfully closed, false otherwise</returns>
     internal static bool StopService()
     {
-      if (_serviceHost != null)
+      if (_serviceHosts != null)
       {
-        //Call StopService from your shutdown logic (i.e. dispose method)
-        if (_serviceHost.State == CommunicationState.Opened)
-        {
-          _serviceHost.Close();
-        }
-
-        var isClosed = (_serviceHost.State == CommunicationState.Closed);
+        var isClosed = WcfUtility.Close(_serviceHosts);
 
         // It appears that Close calls Dispose, then serviceHost must be set to null,
         // so that it get re-instatiated in the StartService method.
         // otherwise calling Open again on a Closed/Disposed service host will fail with the following exception:
         // System.ObjectDisposedException
         //{"Cannot access a disposed object.\r\nObject name: 'System.ServiceModel.ServiceHost'."}
-        _serviceHost = null;
-
+        _serviceHosts = null;
         return isClosed;
       }
-
       return false;
+    }
+    
+    public static ServiceHost CreateDataServiceHost(Type serviceType, string baseAddress)
+    {
+      var fullBaseAddress = string.Concat(baseAddress, serviceType.Name);
+      var host = new DataServiceHost(serviceType, new[] {new Uri(fullBaseAddress)});
+      WcfUtility.DoIncludeExceptionDetailInFaults(host);
+      return host;
     }
   }
 }
