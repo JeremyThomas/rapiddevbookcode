dc2e7c8377de90a174b64e296d4b2d475bd76265
Jeremy Thomas <jeremy_thomas@hotmail.com>
2014-09-14 12:32:45 +0000

    IAdapterQueryableProvider

--- Northwind/DAL/Services/NorthwindPersistence.cs
+++ Northwind/DAL/Services/NorthwindPersistence.cs
@@ -1,12 +1,28 @@
-﻿using Northwind.DAL.HelperClasses;
+﻿using System;
+using System.Linq;
+using Northwind.DAL.HelperClasses;
+using Northwind.DAL.Interfaces;
+using Northwind.DAL.Linq;
+using SD.LLBLGen.Pro.ORMSupportClasses;
 
 namespace Northwind.DAL.Services
 {
   public class NorthwindPersistence : AdapterPersistence
   {
     public NorthwindPersistence()
-      : base(Factories.CreateDataAccessAdapter)
+      : base(Factories.CreateDataAccessAdapter, new NorthwindQueryableProvider())
     {
     }
   }
+
+  public class NorthwindQueryableProvider: IAdapterQueryableProvider
+  {
+
+    public IQueryable<TEntity> GetQueryableForEntity<TEntity>(Func<DataAccessAdapterBase> dataAccessAdapterFactory) where TEntity : class
+    {
+      var linqMetaData = new LinqMetaData(dataAccessAdapterFactory());
+      return linqMetaData.GetQueryableForEntity<TEntity>();
+    }
+  }
+  
 }
