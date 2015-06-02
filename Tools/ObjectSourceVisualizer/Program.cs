ea74d98e6fb29f5e146d61b843f4c18377eab9a5
Jeremy Thomas <jeremy_thomas@hotmail.com>
2015-06-02 12:38:47 +0000

    FormDebuggerVisualizerInstaller(Type dialogVisualizerServiceType, string title, string description)

--- Tools/ObjectSourceVisualizer/Program.cs
+++ Tools/ObjectSourceVisualizer/Program.cs
@@ -15,7 +15,11 @@ namespace ObjectSourceVisualizer
     {
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new FormDebuggerVisualizerInstaller(typeof(IDialogVisualizerService)));
+      var description = "This is an Debugger Visualizer that emits a C# object literal constructor from the C# object chosen in the debugger." + Environment.NewLine +
+                  "To install click on either of the buttons below which will copy this assembly to the directory chosen. " + Environment.NewLine +
+                  "To uninstall go to the folder and remove the assembly." + Environment.NewLine +
+                  "For more info see: https://github.com/jefflomax/csharp-object-to-object-literal";
+      Application.Run(new FormDebuggerVisualizerInstaller(typeof(IDialogVisualizerService), "C# object literal Debugger Visualizer", description));
     }
   }
 }
