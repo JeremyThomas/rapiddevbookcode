339e8dca26919ed76dd47f0111a33400b0cd1d87
Jeremy Thomas <SND\JezzaT_cp>
2015-12-16 11:48:39 +0000

    entity.IsDeleted

--- LLBL Pro v4.2/AW.Winforms.Helpers.LLBL/GeneralEntityCollectionDataScope.cs
+++ LLBL Pro v4.2/AW.Winforms.Helpers.LLBL/GeneralEntityCollectionDataScope.cs
@@ -254,6 +254,12 @@ namespace AW.Winforms.Helpers.LLBL
       if (dataToSave == null) return CommitAllChanges(cascadeDeletes);
       var dataAccessAdapter = TransactionController as IDataAccessAdapter;
       var numberSaved = dataAccessAdapter == null ? EntityHelper.Save(dataToSave) : EntityHelper.Save(dataToSave, dataAccessAdapter, cascadeDeletes);
+      for (var i = _entitiesRemovalTracker.Count - 1; i > -1; i--)
+      {
+        var entity = _entitiesRemovalTracker[i];
+        if (entity.IsDeleted())
+          _entitiesRemovalTracker.Remove(entity);
+      }
       CallEditingFinishedIfNotDirty();
       return numberSaved;
     }
