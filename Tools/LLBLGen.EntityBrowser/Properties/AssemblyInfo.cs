36efa01d0423fb6d5d607188b37d468a01d99857
Jeremy Thomas <jeremy_thomas@hotmail.com>
2016-08-09 08:40:17 +0000

    Comment out AssemblyFileVersion.tt

--- Tools/LLBLGen.EntityBrowser/Properties/AssemblyInfo.cs
+++ Tools/LLBLGen.EntityBrowser/Properties/AssemblyInfo.cs
@@ -8,17 +8,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("LLBLGen Pro Entity Explorer")]
 [assembly: AssemblyDescription("An application that lets you browse, view and edit the data in a database via the mapped LLBLGen Pro entities")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
 [assembly: AssemblyCompany("Jeremy Thomas")]
 [assembly: AssemblyProduct("LLBLGen Pro Entity Explorer")]
 [assembly: AssemblyCopyright("Copyright © Jeremy Thomas 2016")]
 [assembly: AssemblyVersion("1")]
 [assembly: AssemblyInformationalVersion("4.2")]
+
+[assembly: ComVisible(false)]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
